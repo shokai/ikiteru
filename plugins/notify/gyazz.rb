ad5b3ce65abff65c7f090ba91f1bee14573c0a5e
Sho Hashimoto <hashimoto@shokai.org>
2012-01-10 18:12:56 +0900

    add plugin notify/gyazz

---
+++ plugins/notify/gyazz.rb
@@ -0,0 +1,24 @@
+## notify-gyazz.com
+## ikiteru notify plugin
+## http://gyazz.com
+
+### config
+## notify : 
+##  - name : 'gyazz'
+##    title : 'ikiteru'
+##    page : 'log'
+##     => http://gyazz.com/ikiteru/log
+##    basic_user : 'username' (optional)
+##    basic_pass : 'password' (optional)
+
+if status != :alive
+  require 'gyazz'
+  text = "#{addr} => [[[#{status}]]] [#{type}]"
+  page = params['page']
+  begin
+    g = Gyazz.new(params['title'], params['basic_user'], params['basic_pass'])
+    g.set(page, "#{g.get(page)}\n#{text}")
+  rescue => e
+    STDERR.puts e
+  end
+end
